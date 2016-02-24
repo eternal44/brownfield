b030160175f840fec977717d1715c409e9e1a4df
Elliot Chi <elliotschi@gmail.com>
2016-02-23 16:45:15 -0800

    refactor: make angular code into modules

--- client/app/controllers/postsController.js
+++ client/app/controllers/postsController.js
@@ -1,14 +1,13 @@
-module.exports = angular.module('truu.postsController', [])
-  .controller('PostsController', function($scope, $stateParams){
-    $scope.addPost = function(){
-    };
+module.exports = function($scope, $stateParams){
+  $scope.addPost = function(){
+  };
 
-    $scope.incrementVote = function(){
-    };
+  $scope.incrementVote = function(){
+  };
 
-    $scope.decrementVote = function(){
-    };
+  $scope.decrementVote = function(){
+  };
 
-    // $scope.posts = posts.posts[$stateParams.id];
-  });
+  // $scope.posts = posts.posts[$stateParams.id];
+};
 
