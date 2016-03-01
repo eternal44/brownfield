d5090b23b182c960e06fdd4e9d9b5c67f5883c5d
Elliot Chi <elliotschi@gmail.com>
2016-03-01 10:04:51 -0800

    refactor: hello world in react w/ webpack

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,6 @@
 var gulp = require('gulp');
-var connect = require('gulp-connect');
 var karma = require('karma').server;
-var protractor = require('gulp-protractor').protractor;
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
 var eslint = require('gulp-eslint');
-var livereload = require('gulp-livereload');
 
 var paths = {
   scripts: ['client/app/*.js', 'client/app/**/*.js'],
@@ -14,52 +9,12 @@ var paths = {
   test: ['tests/**/*.js']
 };
 
-gulp.task('connect', function() {
-  connect.server({
-    root: 'client/',
-    port: 8888
-  });
-});
-
 gulp.task('unit', function (done) {
   karma.start({
     configFile: __dirname + '/tests/karma.conf.js',
     singleRun: true
   }, done);
 });
-
-gulp.task('e2e', function() {
-  var args = ['--baseUrl', 'http://127.0.0.1:8888'];
-  gulp.src(['./tests/e2e/*.js'])
-    .pipe(protractor({
-      configFile: 'tests/protractor.conf.js',
-      args: args
-    }))
-    .on('error', function(err) { throw err; });
-});
-
-gulp.task('browserify', function() {
-  return browserify('./client/app/app.js')
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest('./client/build/'))
-    .pipe(livereload());
-});
-
-// gulp.task('build-css', function() {
-//   return gulp.src(paths.styles)
-//     .pipe(sass())
-//     .pipe(gulp.dest('./client/build/'))
-//     .pipe(livereload());
-// });
-
-// gulp.task('views', function() {
-//   gulp.src('client/index.html')
-//   .pipe(gulp.dest('./client/build/'));
-
-//   gulp.src('./client/views/**/*')
-//   .pipe(gulp.dest('./client/views/'));
-// });
 
 gulp.task('eslint', function() {
   return gulp.src(['**/*.js', '!tests/*.js', '!node_modules/**/*.js'])
@@ -69,12 +24,3 @@ gulp.task('eslint', function() {
   .pipe(eslint.format())
   .pipe(eslint.failAfterError());
 });
-
-gulp.task('watch', function() {
-  livereload.listen();
-
-  gulp.watch(paths.scripts, ['browserify']);
-  gulp.watch(paths.html, ['browserify']);
-});
-
-gulp.task('default', ['browserify', 'watch']);
--- client/app/config/ngMaterialConfig.js
+++
@@ -1,7 +0,0 @@
-module.exports = function($mdThemingProvider) {
-  $mdThemingProvider.theme('default')
-    .primaryPalette('purple')
-    .accentPalette('blue', {
-      'default': '200' 
-    });
-}
