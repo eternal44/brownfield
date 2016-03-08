0455b438f78dc15d50201d8168cb3cd7415e8779
Elliot Chi <elliotschi@gmail.com>
2016-03-07 16:12:48 -0800

    feat: change id database schema to serials

--- src/components/app.js
+++ src/components/app.js
@@ -4,15 +4,16 @@ import SearchBar from './searchBar';
 import Auth from './auth';
 import PostList from '../containers/post-list';
 import PostDetail from '../containers/post-detail';
+import NavBar from './navbar';
 
 export default class App extends Component {
 
   render() {
     return (
       <div>
+        <NavBar />
         <h3>truu!</h3>
-         <Auth />
-
+         <SearchBar />
          <PostList />
          <PostDetail />
       </div>
