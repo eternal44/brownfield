545c6a42bb2d294d178506b4aaf891cbb8387e3f
Elliot Chi <elliotschi@gmail.com>
2016-03-21 20:22:17 -0700

    feat: write out skeleton for vote model

--- server/db/models/voteModel.js
+++ server/db/models/voteModel.js
@@ -1,16 +1,23 @@
 import db from '../db';
+import Promise from 'bluebird';
 
 export default {
   // db query for user to vote for a post => left/right swipe
   userVoteForPost: (userId, postId, voteType) => {
-
+    return new Promise((resolve, reject) => {
+      let queryString = ``;
+    });
   },
   // db query to update vote for post if you made a mistake voting
   updateVoteForPost: (userId, postId, voteType) => {
-
+    return new Promise((resolve, reject) => {
+      let queryString = ``;
+    });
   },
-  // function that gives you back the net rating for a post to decide wheter you want it or not
+  // function that gives you back the net rating for a post to decide whether you want it or not
   getVotesForPost: () => {
-
+    return new Promise((resolve, reject) => {
+      let queryString = ``;
+    });
   }
 }
