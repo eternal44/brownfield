6727f06bb6e9607eba85401a141d9be246088978
Elliot Chi <elliotschi@gmail.com>
2016-03-28 11:25:26 -0700

    feat: lay out skeleton for vote controller functions

--- server/controllers/voteController.js
+++ server/controllers/voteController.js
@@ -1,14 +1,41 @@
 import Vote from '../db/models/voteModel';
+import { isEmpty } from 'lodash';
 
 export default {
-  voteGet: (req, res) => {
-    res.json({
-      voteType: true
-    });
+  /* function for vote get request for a post  => gets back all votes for a 
+  single post*/
+  voteGetForAPost: (req, res, next) => {
+    if (req.params.postId) {
+      
+    } else {
+      next(new Error('must pass in a post id as a url parameter'));
+    }
   },
-  votePost: (req, res) => {
+  
+  /* function for vote get request for a user => gets back all votes that a user 
+  has posted (could be used for vote history?) */
+  voteGetForAUser: (req, res, next) => {
+    
+  },
+  
+  /* function for vote post request by a voter to a post => creates an entry in 
+  the database with the vote weight property which is a number (positive or negative)  this number is decided by 
+  a credibility for voting that they build up to */
+  votePost: (req, res, next) => {
     res.json({
       voteType: false
     });
-  }
+  },
+  
+  /* function for vote put request: for if a user makes a mistake on voting or if 
+  they change their mind about some vote they posted. required params userId, postId, voteWeight*/
+  votePut: (req, res, next) => {
+    
+  },
+  
+  /* function for vote delete request: when a user wants to delete their 
+  vote... not sure when this will be used yet */
+  voteDelete: (req, res, next) => {
+    
+  } 
 }
