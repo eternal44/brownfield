6131c1b87cbac4d7d0ed0a824af9b78d93cb5613
Elliot Chi <elliotschi@gmail.com>
2016-03-28 11:54:41 -0700

    feat: finish vote controller and routes

--- server/routes/voteRoutes.js
+++ server/routes/voteRoutes.js
@@ -1,6 +1,9 @@
 import voteController from '../controllers/voteController';
 
 export default router => {
-  router.get('/', voteController.voteGet);
+  router.get('/post/:postId', voteController.voteGetForAPost);
+  router.get('/user/:userId', voteController.voteGetForAUser);
   router.post('/', voteController.votePost);
+  router.put('/', voteController.votePut);
+  router.delete('/', voteController.voteDelete);
 }
