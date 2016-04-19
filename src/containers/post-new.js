d78c5b9a9dfd95995062a39fb5e8fe16583de159
Elliot Chi <elliotschi@gmail.com>
2016-04-19 10:42:16 -0700

    chore: add back context types

--- src/containers/post-new.js
+++ src/containers/post-new.js
@@ -4,7 +4,9 @@ import { createPost } from '../actions';
 import { Link } from 'react-router';
 
 class PostsNew extends Component {
-  
+  static contextTypes = {
+    router: PropTypes.object
+  }
   
   onSubmit(props) {
     this.props.createPost(props)
